c89e984ad005352d325fc2c36684c5c577c70c04
fischerjoao <joao-fischer@hotmail.com>
2025-06-01 13:17:57 -0300

    🚚feat: login structure

---
+++ app/context/AuthContext.tsx
@@ -0,0 +1,7 @@
+
+interface AuthProps {
+    authState?:{token: string | null, authenticated: boolean | null};
+    onRegister?: (email: string, password: string) => Promise<any>;
+    onLogin?: (email: string, password: string) => Promise<any>;
+    onLogout?: () => Promise<any>;
+}
