e6d62a8dbc7f3acd2fe584a969d96cfa84efefc6
fischerjoao <joao-fischer@hotmail.com>
2025-06-02 22:18:09 -0300

    ✨ feat: Implement authentication check in RootLayout with redirect to login

--- app/(protected)/_layout.tsx
+++ app/(protected)/_layout.tsx
@@ -1,14 +1,20 @@
-import { Slot } from "expo-router";
+import { Redirect, Slot } from "expo-router";
 import React, { useState } from "react";
 import { StatusBar } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
-import Footer from "../components/Footer";
-import Menu from "../components/Menu";
-import NavBar from "../components/Navbar";
+import Footer from "../../components/Footer";
+import Menu from "../../components/Menu";
+import NavBar from "../../components/Navbar";
+import { useAuth } from "../../hooks/useAuth"; // use o hook aqui
 import BotaoChat from "../components/chat/BotaoChat";
 
 export default function RootLayout() {
   const [menuVisible, setMenuVisible] = useState(false);
+  const { authState } = useAuth();
+
+  if (!authState.authenticated) {
+    return <Redirect href="/login" />;
+  }
 
   return (
     <SafeAreaView
@@ -17,11 +23,7 @@ export default function RootLayout() {
     >
       <StatusBar barStyle="light-content" backgroundColor="#2F4172" />
       <NavBar onMenuPress={() => setMenuVisible(true)} />
-
-      {/* This Slot component is crucial for Expo Router to render the child routes */}
       <Slot />
-
-      {/* Menu lateral */}
       <Menu visible={menuVisible} onClose={() => setMenuVisible(false)} />
       <Footer />
       <BotaoChat />
