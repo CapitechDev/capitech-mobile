eca8ea85452383a92a26ed9347554d11cb352485
fischerjoao <joao-fischer@hotmail.com>
2025-06-08 19:59:59 -0300

    ♻️ Refactor: Organize imports and clean up HomeScreen component layout

--- app/(protected)/index.tsx
+++ app/(protected)/index.tsx
@@ -1,12 +1,16 @@
+import { Link } from "expo-router";
 import React from "react";
-
-import { ScrollView, StyleSheet, Text, View } from "react-native";
-
+import {
+  Image,
+  ScrollView,
+  StyleSheet,
+  Text, View
+} from "react-native";
 
 export default function HomeScreen() {
   return (
     <ScrollView contentContainerStyle={styles.scrollContainer}>
-      <View style={[styles.container]}>
+      <View style={styles.container}>
         <Text style={styles.title}>Bem-vindo ao CapiTech</Text>
 
         <View style={[styles.card, { backgroundColor: "#1EA0D1" }]}>
@@ -97,7 +101,6 @@ const styles = StyleSheet.create({
     flex: 1,
     padding: 20,
     alignItems: "center",
-    gap: 7,
   },
   title: {
     fontSize: 24,
@@ -105,5 +108,37 @@ const styles = StyleSheet.create({
     color: "#2F4172",
     marginBottom: 20,
     marginTop: 10,
+  },
+  card: {
+    backgroundColor: "white",
+    borderRadius: 10,
+    padding: 20,
+    marginVertical: 10,
+    width: "100%",
+    shadowColor: "#000",
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.1,
+    shadowRadius: 4,
+    elevation: 3,
+  },
+  cardRow: {
+    flexDirection: "row",
+    alignItems: "center",
+  },
+  cardTitle: {
+    fontSize: 18,
+    fontWeight: "bold",
+    color: "#2F4172",
+    marginBottom: 10,
+  },
+  cardContent: {
+    fontSize: 16,
+    color: "#555",
+  },
+  icon: {
+    width: 55,
+    height: 55,
+    borderRadius: 10,
+    marginRight: 1,
   },
 });
